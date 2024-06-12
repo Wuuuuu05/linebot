1ebc48b76346fa5bc17a6bf541fe62a8a118f5df
Wuuuuu05 <127123258+Wuuuuu05@users.noreply.github.com>
2024-06-12 21:04:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
+import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, RichMenu, RichMenuArea, RichMenuBounds, RichMenuSize, URIAction
 
 app = Flask(__name__)
 
-# 設置您的Channel Secret和Channel Access Token
-line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('CHANNEL_SECRET')
+# Line bot credentials from environment variables
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -24,36 +32,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == 'menu':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='這是圖文選單範例')
-        )
-        create_rich_menu()
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 def create_rich_menu():
-    rich_menu_to_create = RichMenu(
+    rich_menu = RichMenu(
         size=RichMenuSize(width=2500, height=1686),
-        selected=False,
-        name="Nice richmenu",
-        chat_bar_text="Tap here",
+        selected=True,
+        name="Menu",
+        chat_bar_text="Tap to open",
         areas=[
             RichMenuArea(
                 bounds=RichMenuBounds(x=0, y=0, width=1250, height=843),
-                action=MessageAction(label='Hello', text='Hello')
+                action=URIAction(uri='https://example.com/')
             ),
             RichMenuArea(
-                bounds=RichMenuBounds(x=1251, y=0, width=1250, height=843),
-                action=MessageAction(label='World', text='World')
+                bounds=RichMenuBounds(x=1250, y=0, width=1250, height=843),
+                action=URIAction(uri='https://example.com/')
             )
         ]
     )
-    rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
-
-    with open("path_to_your_image.png", 'rb') as f:
-        line_bot_api.set_rich_menu_image(rich_menu_id, "image/png", f)
-
+    rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu)
+    with open('path/to/rich_menu_image.png', 'rb') as f:
+        line_bot_api.set_rich_menu_image(rich_menu_id, 'image/png', f)
     line_bot_api.set_default_rich_menu(rich_menu_id)
 
 if __name__ == "__main__":
-    app.run()
+    create_rich_menu()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
