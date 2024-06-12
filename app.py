9489ea24749528882ccfdd16aa659dee6232efda
Wuuuuu05 <127123258+Wuuuuu05@users.noreply.github.com>
2024-06-12 19:37:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from linebot.models import *
 app = Flask(__name__)
 
 # 設置您的Channel Secret和Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
