26e3527cd9615fffcb12f0fb18511660176576b4
Wuuuuu05 <127123258+Wuuuuu05@users.noreply.github.com>
2024-06-13 14:09:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,38 +43,38 @@ def create_rich_menu():
 
     body = json.dumps({
         'size': {'width': 2500, 'height': 1686},   # 設定尺寸
-    'selected': 'true',                        # 預設是否顯示
-    'name': 'Richmenu demo',                   # 選單名稱
-    'chatBarText': 'Richmenu demo',            # 選單在 LINE 顯示的標題
-    'areas':[                                  # 選單內容
-        {
-          'bounds': {'x': 341, 'y': 75, 'width': 560, 'height': 340}, # 選單位置與大小
-          'action': {'type': 'message', 'text': '電器'}                # 點擊後傳送文字
-        },
-        {
-          'bounds': {'x': 1434, 'y': 229, 'width': 930, 'height': 340},
-          'action': {'type': 'message', 'text': '運動用品'}
-        },
-        {
-          'bounds': {'x': 122, 'y': 641, 'width':560, 'height': 340},
-          'action': {'type': 'message', 'text': '客服'}
-        },
-        {
-          'bounds': {'x': 1012, 'y': 645, 'width': 560, 'height': 340},
-          'action': {'type': 'message', 'text': '餐廳'}
-        },
-        {
-          'bounds': {'x': 1813, 'y': 677, 'width': 560, 'height': 340},
-          'action': {'type': 'message', 'text': '鞋子'}
-        },
-        {
-          'bounds': {'x': 423, 'y': 1203, 'width': 560, 'height': 340},
-          'action': {'type': 'message', 'text': '美食'}
-        },
-        {
-          'bounds': {'x': 1581, 'y': 1133, 'width': 560, 'height': 340},
-          'action': {'type': 'message', 'text': '衣服'}
-        }
+        'selected': 'true',                        # 預設是否顯示
+        'name': 'Richmenu demo',                   # 選單名稱
+        'chatBarText': 'Richmenu demo',            # 選單在 LINE 顯示的標題
+        'areas':[                                  # 選單內容
+            {
+              'bounds': {'x': 341, 'y': 75, 'width': 560, 'height': 340}, # 選單位置與大小
+              'action': {'type': 'message', 'text': '電器'}                # 點擊後傳送文字
+            },
+            {
+              'bounds': {'x': 1434, 'y': 229, 'width': 930, 'height': 340},
+              'action': {'type': 'message', 'text': '運動用品'}
+            },
+            {
+              'bounds': {'x': 122, 'y': 641, 'width':560, 'height': 340},
+              'action': {'type': 'message', 'text': '客服'}
+            },
+            {
+              'bounds': {'x': 1012, 'y': 645, 'width': 560, 'height': 340},
+              'action': {'type': 'message', 'text': '餐廳'}
+            },
+            {
+              'bounds': {'x': 1813, 'y': 677, 'width': 560, 'height': 340},
+              'action': {'type': 'message', 'text': '鞋子'}
+            },
+            {
+              'bounds': {'x': 423, 'y': 1203, 'width': 560, 'height': 340},
+              'action': {'type': 'message', 'text': '美食'}
+            },
+            {
+              'bounds': {'x': 1581, 'y': 1133, 'width': 560, 'height': 340},
+              'action': {'type': 'message', 'text': '衣服'}
+            }
         ]
     })
     
